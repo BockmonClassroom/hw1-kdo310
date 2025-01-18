a5e3e77e9ca07d1319326fba6779bf19f3d74015
donahuekel <donahue.kel@northeastern.edu>
2025-01-17 19:36:37 -0500

    hw1

---
+++ hw1.py
@@ -0,0 +1,9 @@
+#Kelsey Donahue
+#HW1
+#1/17/2025
+import pandas as pd
+
+#reading in the csv using the file path
+data = pd.read_csv("/Users/kelsey/Desktop/DS5110/hw1-kdo310/data/Iris.csv")
+#printing the data
+print(data)
